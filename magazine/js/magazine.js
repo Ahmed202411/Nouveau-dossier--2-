f21d028680fcbf0fea6b8c6045374aae064d4882
Ahmed202411 <ahmedlabghial@gmail.com>
2024-12-13 00:24:39 +0100

    Update magazine.js

--- magazine/js/magazine.js
+++ magazine/js/magazine.js
@@ -48,7 +48,7 @@ function loadPage(page, pageElement) {
 
 	// Load the page
 
-	img.attr('src', 'pages/' + page + '.jpg');
+	img.attr('src', 'https://ahmed202411.github.io/Nouveau-dossier--2-/magazine/pages/' + page + '.jpg');
 
 	loadRegions(page, pageElement);
 
@@ -179,7 +179,7 @@ function loadLargePage(page, pageElement) {
 
 	// Loadnew page
 
-	img.attr('src', 'pages/' + page + '-large.jpg');
+	img.attr('src', 'https://ahmed202411.github.io/Nouveau-dossier--2-/magazine/pages/' + page + '-large.jpg');
 }
 
 // Load small page
@@ -193,7 +193,7 @@ function loadSmallPage(page, pageElement) {
 	img.off('load');
 	// Loadnew page
 
-	img.attr('src', 'pages/' + page + '.jpg');
+	img.attr('src', 'https://ahmed202411.github.io/Nouveau-dossier--2-/magazine/pages/' + page + '.jpg');
 }
 
 function disableControls(page) {
